2383a84c608bad77c32a7807369daf9195fce2dc
Edcel Jeremy Macawili <edcel.macawili@dottystylecreative.com>
2025-07-14 13:50:47 +0800

    initial commit setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,54 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Appearance } from "react-native";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Colors } from "@/constants/Colors";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+	const colorScheme = Appearance.getColorScheme();
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+	const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+	const [loaded] = useFonts({
+		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+	});
+
+	if (!loaded) {
+		// Async font loading only occurs in development.
+		return null;
+	}
+
+	return (
+		<Stack
+			screenOptions={{
+				headerStyle: { backgroundColor: theme.headerBackground },
+				headerTintColor: theme.text,
+				headerShadowVisible: false,
+			}}
+		>
+			<Stack.Screen
+				name="index"
+				options={{ title: "Home", headerShown: false }}
+			/>
+			<Stack.Screen
+				name="Shop"
+				options={{
+					title: "Explore",
+					headerShown: true,
+					headerTitle: "Shop Screen",
+				}}
+			/>
+
+			<Stack.Screen
+				name="explore"
+				options={{
+					title: "Explore",
+					headerShown: true,
+					headerTitle: "Explore Screen",
+				}}
+			/>
+			<Stack.Screen name="+not-found" />
+		</Stack>
+	);
 }
---
+++ app/index.tsx
@@ -0,0 +1,68 @@
+import {
+	View,
+	Text,
+	StyleSheet,
+	ImageBackground,
+	Pressable,
+} from "react-native";
+import { Link } from "expo-router";
+
+import gundamImg from "@/assets/images/gundam.png";
+
+const app = () => {
+	return (
+		<View style={styles.container}>
+			<ImageBackground
+				source={gundamImg}
+				resizeMode="cover"
+				style={styles.image}
+			>
+				<Text style={styles.text}>Gundam Shop</Text>
+				<Link href="/explore" style={{ marginHorizontal: "auto" }} asChild>
+					<Pressable style={styles.button}>
+						<Text style={styles.buttonText}>Check items</Text>
+					</Pressable>
+				</Link>
+			</ImageBackground>
+		</View>
+	);
+};
+
+export default app;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: "column",
+		justifyContent: "center",
+	},
+	text: {
+		color: "white",
+		fontSize: 40,
+		fontWeight: "light",
+		textAlign: "center",
+	},
+	image: {
+		width: "100%",
+		height: "100%",
+		flex: 1,
+		resizeMode: "cover",
+		justifyContent: "center",
+	},
+	link: {
+		color: "white",
+		fontSize: 20,
+		textAlign: "center",
+		marginTop: 20,
+	},
+	button: {
+		borderRadius: 20,
+		backgroundColor: "rgba(255, 255, 255, 0.5)",
+	},
+	buttonText: {
+		color: "black",
+		fontSize: 20,
+		textAlign: "center",
+		margin: 20,
+	},
+});
