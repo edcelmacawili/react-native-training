084e185018e7fe2da009b4b3d98dd3054e83c9a7
Edcel Jeremy Macawili <edcel.macawili@dottystylecreative.com>
2025-07-23 11:34:30 +0800

    chapter 6

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,35 +1,15 @@
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import { Appearance } from "react-native";
-
-import { Colors } from "@/constants/Colors";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ThemeProvider } from "../context/ThemeContext";
 
 export default function RootLayout() {
-	const colorScheme = Appearance.getColorScheme();
-
-	const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
-
-	const [loaded] = useFonts({
-		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-	});
-
-	if (!loaded) {
-		// Async font loading only occurs in development.
-		return null;
-	}
-
 	return (
-		<Stack
-			screenOptions={{
-				headerStyle: { backgroundColor: theme.headerBackground },
-				headerTintColor: theme.text,
-			}}
-		>
-			<Stack.Screen
-				name="index"
-				options={{ title: "Home", headerShown: false }}
-			/>
-			<Stack.Screen name="+not-found" />
-		</Stack>
+		<ThemeProvider>
+			<SafeAreaProvider>
+				<Stack>
+					<Stack.Screen name="index" options={{ headerShown: false }} />
+				</Stack>
+			</SafeAreaProvider>
+		</ThemeProvider>
 	);
 }
